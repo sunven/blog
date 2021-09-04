b3b4abe406edbdb012dc21e5b808af1d0aa142a2
sunven <604164227@qq.com>
2021-09-04 09:43:25 +0800

    add math

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,23 @@ module.exports = {
   //debug: true,
   // title: '你好， VuePress ！',
   // description: '这是我的第一个 VuePress 站点',
+  head: [
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdn.jsdelivr.net/npm/github-markdown-css@4.0.0/github-markdown.min.css',
+      },
+    ],
+  ],
   plugins: [
     [
       '@vuepress/register-components',
@@ -17,9 +34,27 @@ module.exports = {
         componentsDir: path.resolve(__dirname, './components'),
       },
     ],
+    [
+      'vuepress-plugin-mathjax',
+      {
+        target: 'svg',
+        macros: {
+          '*': '\\times',
+        },
+      },
+    ],
   ],
   themeConfig: {
     navbar,
     sidebar,
   },
+  extendsMarkdown: md => {
+    md.use(require('./plugins/markdown-it-katex'))
+    //md.linkify.set({ fuzzyEmail: false })
+  },
+  // markdown: {
+  //   extendMarkdown: md => {
+  //     md.use(require('markdown-it-katex'))
+  //   },
+  // },
 }
---
+++ docs/tool/mac/on-my-zsh.md
@@ -0,0 +1,12 @@
+# on-my-zsh
+
+主题：
+
+https://github.com/romkatv/powerlevel10k
+
+插件：
+
+https://github.com/zsh-users/zsh-syntax-highlighting
+
+https://github.com/zsh-users/zsh-autosuggestions
+
